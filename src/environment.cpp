a7ef28df48d45965aa8add7287bfda440276d071
FabricioNick <fabriciotumialan143@gmail.com>
2026-09-09 23:42:41 -0500

    feat: implement game termination rules (#12)

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,3 +1,4 @@
+#include "circuit_escape/environment.h"
 #include "circuit_escape/agent.h"
 #include "circuit_escape/game_rules.h"
 
@@ -12,5 +13,25 @@ namespace {
             -rules.trapScorePenalty
         );
     }
+    EndReason evaluateTermination(
+        bool agentOnExit,
+        int energy,
+        std::size_t turn,
+        std::size_t turnLimit
+    ) noexcept {
 
+        if (agentOnExit && energy > 0) {
+            return EndReason::goalReached;
+        }
+
+        if (energy == 0) {
+            return EndReason::noEnergy;
+        }
+
+        if (turn >= turnLimit) {
+            return EndReason::turnLimit;
+        }
+
+        return EndReason::none;
+    }
 }
