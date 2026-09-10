2cf1b906db350ca38c8ba2a6925a4c6133c7d2a5
jared <jaredchalas@gmail.com>
2026-09-10 17:20:35 -0500

    feat(controllers): implementar adaptador PolicyController
    - Se crea la clase template PolicyController que hereda de IController.
    - Se restringe el template utilizando el concept NavigationPolicy de C++20.
    - Se implementa el constructor con move semantics (std::move) para la inicialización eficiente de la política.
    - Se sobrescribe selectAction para delegar la toma de decisiones al método de la politica interna

--- include/circuit_escape/controllers.h
+++ include/circuit_escape/controllers.h
@@ -1,20 +1,24 @@
 #pragma once
 
 #include "circuit_escape/environment.h"
-
 #include <span>
 #include <concepts>
-
+#include <utility>
+/*
+- clase abstracta de la que heredan todos los controllers
+- destructor virtual necesario y todas las funciones abtractas
+*/
 class IController {
 public:
     virtual ~IController() = default;
-
-    virtual Action selectAction(
-        const Observation& observation,
-        std::span<const Action> legalActions
-    ) = 0;
+    virtual Action selectAction(const Observation& observation, std::span<const Action> legalActions) = 0;
 };
-
+/*
+Concept que verifica que el tipo de dato Policy tenga un metodo un metodo llamada selectAction 
+y verifica que este bien implementado: seleciona una accion que sea
+std::span() objeto ligero hacia una lista contigua de elementos
+std::same_As() verifica si los 2 tipos son exacatamente iguales
+*/
 template<typename Policy>
 concept NavigationPolicy = requires(
     Policy& policy,
@@ -22,4 +26,19 @@ concept NavigationPolicy = requires(
     std::span<const Action> actions
 ) {
     { policy.selectAction(observation, actions) }-> std::same_as<Action>;
+};
+
+
+template<typename Policy>
+requires NavigationPolicy<Policy>
+class PolicyController: public IController {
+    Policy policy_;
+public:
+    //explicit es una buena practica para eivtar conversiones impliucitas
+    // se usa la asignacion por movimiento por eficiencia (una copia seria innecesaria pues no necesitamos mantener el parametro policy despues del contructor)
+    //PolicyController posee su propia politica (policy)
+    explicit PolicyController(Policy policy) : policy_(std::move(policy)) {}
+    Action selectAction(const Observation& observation, std::span<const Action> legalActions) override {
+        return policy_.selectAction(observation,legalActions);
+    }
 };
