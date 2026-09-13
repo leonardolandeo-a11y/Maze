7df60e05d00aa3b71a159a102694f64eb84469f7
SRHouse <fabriciotumialan143@gmail.com>
2026-09-13 09:27:41 -0500

    feat: add cinematic startup animation for Maze (#71)
    * build: fix FTXUI linkage for Maze core
    
    * feat: add cinematic startup boot sequence
    
    * feat: add maze scan and digital interference
    
    * feat: complete cinematic startup animation

--- include/circuit_escape/GameApplication.h
+++ include/circuit_escape/GameApplication.h
@@ -1,17 +1,26 @@
 #pragma once
+
 #include "circuit_escape/environment.h"
 #include "circuit_escape/cells.h"
 #include "circuit_escape/grid.h"
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/component/event.hpp>
 #include "circuit_escape/agent.h"
 #include "circuit_escape/console_ui.h"
+
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/event.hpp>
 #include <ftxui/component/component.hpp>
+
 class GameApplication{
 private:
     Grid<Cell, 20,30> CreateScenario();
     Agent CreatePlayer();
-    ftxui::Component CreateGameComponent(NavigationEnvironment<20,30>& environment,ConsoleUI& ui, std::vector<NavigationEvent>& recentEvents, ftxui::ScreenInteractive& screen);
+
+    ftxui::Component CreateGameComponent(
+        NavigationEnvironment<20,30>& environment,
+        ConsoleUI& ui,
+        std::vector<NavigationEvent>& recentEvents,
+        ftxui::ScreenInteractive& screen
+    );
 
 public:
     void Run();
